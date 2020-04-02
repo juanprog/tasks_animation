81ace41d0fa01223985157e92ac3428e9a6c910c
Juan Carlos <jcguitar1994@gmail.com>
2020-04-02 17:30:08 -0300

    Configuring first animation of button

--- lib/screens/login/widgets/stagger_animation.dart
+++ lib/screens/login/widgets/stagger_animation.dart
@@ -2,34 +2,55 @@ import 'package:flutter/material.dart';
 
 class StaggerAnimation extends StatelessWidget {
   final AnimationController controller;
+  final Animation<double> buttonSqueeze;
 
-  StaggerAnimation({this.controller});
+  StaggerAnimation({this.controller})
+      : buttonSqueeze = Tween<double>(
+          begin: 250,
+          end: 40,
+        ).animate(CurvedAnimation(
+          parent: controller,
+          curve: Interval(0.0, 0.150),
+        ));
 
   Widget _buildAnimation(BuildContext context, Widget child) {
     return Padding(
       padding: EdgeInsets.only(bottom: 45),
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          controller.forward();
+        },
         child: Container(
-          width: 250,
+          width: buttonSqueeze.value,
           height: 40,
           alignment: Alignment.center,
           decoration: BoxDecoration(
             color: Color.fromRGBO(255, 161, 27, 1.0),
             borderRadius: BorderRadius.all(Radius.circular(30.0)),
           ),
-          child: Text(
-            "Sign in",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 20,
-              fontWeight: FontWeight.w300,
-              letterSpacing: 0.3,
-            ),
-          ),
+          child: _buildInside(context),
         ),
       ),
     );
+  }
+
+  Widget _buildInside(BuildContext context) {
+    if (buttonSqueeze.value > 75) {
+      return Text(
+        "Sign in",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.w300,
+          letterSpacing: 0.3,
+        ),
+      );
+    } else {
+      return CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+        strokeWidth: 1.0,
+      );
+    }
   }
 
   @override
