3618711c69861b6ffb31680a345cf209cb5d0d1a
Juan Carlos <jcguitar1994@gmail.com>
2020-04-02 16:12:15 -0300

    file form container

---
+++ lib/screens/login/widgets/form_container.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:tasks_animation/screens/login/widgets/input_field.dart';
+
+class FormContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20),
+      child: Form(
+        child: Column(
+          children: <Widget>[
+            InputField(
+              hint: "Username",
+              obscure: false,
+              icon: Icons.person_outline,
+            ),
+            InputField(
+              hint: "Password",
+              obscure: true,
+              icon: Icons.lock_outline,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
